46b74cf430c3051cc8d827318bb25f8740c0f2d7
Marcos Hobson <marcoshobson@gmail.com>
2020-05-14 12:41:15 -0300

    Adicionando teclas de atalho e exibindo o botão excluir ao adicionar tarefa

--- src/todo/todoFrom.jsx
+++ src/todo/todoFrom.jsx
@@ -2,22 +2,37 @@ import React from 'react'
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
-export default props => (
-    <div role='form' className='todoForm'>
-        <Grid cols='12 9 10'>
-            <input id='description' className='form-control'
-                placeholder='Adicione uma tarefa'
-                onChange={props.handleChange}
-                value={props.description}></input>
-        </Grid>
+export default props => {
+    const keyHandler = (e) => {
+        if (e.key === 'Enter') {
+            props.handleAdd()   
+        } else if (e.key === 'F2') {
+            props.handleSearch()
 
-        <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus'
-                onClick={props.handleAdd}></IconButton>
-            <IconButton style='info' icon='search'
-                onClick={props.handleSearch}></IconButton>
-            <IconButton style='default' icon='close'
-                onClick={props.handleClear}></IconButton>
-        </Grid>
-    </div>
-)
+        } else if (e.key === 'Escape') {
+            props.handleClear()
+        }
+    }
+
+    return (
+        <div role='form' className='todoForm'>
+            <Grid cols='12 9 10'>
+                <input id='description' className='form-control'
+                    placeholder='Digite para adicionar ou pesquisar uma tarefa'
+                    title='Enter = Adicionar, F2 = Pesquisar, Esc = Limpar'
+                    onChange={props.handleChange}
+                    onKeyUp={keyHandler}
+                    value={props.description}></input>
+            </Grid>
+    
+            <Grid cols='12 3 2'>
+                <IconButton style='primary' icon='plus' title='Adicionar'
+                    onClick={props.handleAdd}></IconButton>
+                <IconButton style='info' icon='search' title='Pesquisar'
+                    onClick={props.handleSearch}></IconButton>
+                <IconButton style='default' icon='close' title='Limpar'
+                    onClick={props.handleClear}></IconButton>
+            </Grid>
+        </div>
+    )
+}
--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -9,10 +9,13 @@ export default props => {
                 <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                 <td>
                     <IconButton style='success' icon='check' hide={todo.done}
+                        title='Marcar como concluído'
                         onClick={() => props.handleMarkAsDone(todo)}></IconButton>
                     <IconButton style='warning' icon='undo' hide={!todo.done}
+                        title="Desmarcar concluído"
                         onClick={() => props.handleMarkAsPending(todo)}></IconButton>
-                    <IconButton style='danger' icon='trash-o' hide={!todo.done}
+                    <IconButton style='danger' icon='trash-o' hide={todo.done}
+                        title='Remover tarefa'
                         onClick={() => props.handleRemove(todo)}></IconButton>
                 </td>
             </tr>
